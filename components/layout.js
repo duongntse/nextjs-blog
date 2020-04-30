a35e08a952b5e717f4408a69aac76849e7e69d47
Harry Nguyen <duongntse@gmail.com>
2020-04-30 16:12:12 +0700

    update layout.js

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Harry';
+const name = 'Harry Happyness';
 const imgProfile = '/images/harry.jpg';
 export const siteTitle = 'Next.js Sample website';
 
